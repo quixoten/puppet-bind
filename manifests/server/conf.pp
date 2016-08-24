ca748ccb122ec138bd5e173a5c68163c7328d1f0
George Lambson <george.lambson@mx.com>
2016-08-24 15:13:42 -0600

    Set to undef

--- manifests/server/conf.pp
+++ manifests/server/conf.pp
@@ -104,7 +104,7 @@ define bind::server::conf (
   $dump_file              = '/var/named/data/cache_dump.db',
   $statistics_file        = '/var/named/data/named_stats.txt',
   $memstatistics_file     = '/var/named/data/named_mem_stats.txt',
-  $statistics_channels    = '',
+  $statistics_channels    = undef,
   $allow_query            = [ 'localhost' ],
   $allow_query_cache      = [],
   $recursion              = 'yes',
